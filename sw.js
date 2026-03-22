4e103be2f6c4d480be42349b4d882380d1376ebe
sscjourneytest <sscgkgspreparation@gmail.com>
2026-03-22 23:37:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,15 @@
-const CACHE_NAME = 'mockmatrix-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'mock-matrix-v1';
 
-// Install: Caches the basic files and forces the new version to take over
+// Install event
 self.addEventListener('install', (event) => {
-  self.skipWaiting(); 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+    self.skipWaiting();
 });
 
-// Activate: Cleans up old versions automatically when you deploy a new update
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Fetch: Makes the app work even with a slow internet connection
+// Fetch event (Always gets latest from web - Auto-Update)
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
