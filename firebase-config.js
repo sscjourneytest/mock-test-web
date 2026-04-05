1f7eef2d9921bec9f7b632b33d72fec8c3c8f1d3
sscjourneytest <sscgkgspreparation@gmail.com>
2026-04-05 17:39:10 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const FIREBASE_PROJECTS = {
+    cgl: {
+        apiKey: "AIzaSyBlYDQQ-tqx57iILNP70p08dsKd03aXoO4",
+        authDomain: "mmh-cgl-full-mock-data-1.firebaseapp.com",
+        databaseURL: "https://mmh-cgl-full-mock-data-1-default-rtdb.firebaseio.com",
+        projectId: "mmh-cgl-full-mock-data-1",
+        storageBucket: "mmh-cgl-full-mock-data-1.firebasestorage.app",
+        messagingSenderId: "155878036031",
+        appId: "1:155878036031:web:a061644eea5c97dfaa5e8a"
+    }
+};
