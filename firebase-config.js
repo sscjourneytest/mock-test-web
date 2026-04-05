0ad13c7555adffd43b5d4febab8f983de1d1200d
sscjourneytest <sscgkgspreparation@gmail.com>
2026-04-05 20:47:18 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,5 +8,14 @@ const FIREBASE_PROJECTS = {
         storageBucket: "mmh-cgl-full-mock-data-1.firebasestorage.app",
         messagingSenderId: "155878036031",
         appId: "1:155878036031:web:a061644eea5c97dfaa5e8a"
-    }
+    },
+    chsl: {
+  apiKey: "AIzaSyCekzDObGN7Gc5ENfmMejddM8PKxOob_gE",
+  authDomain: "mmh-chsl-full-mock-data.firebaseapp.com",
+  databaseURL: "https://mmh-chsl-full-mock-data-default-rtdb.firebaseio.com",
+  projectId: "mmh-chsl-full-mock-data",
+  storageBucket: "mmh-chsl-full-mock-data.firebasestorage.app",
+  messagingSenderId: "672897736088",
+  appId: "1:672897736088:web:ddff435bec20076ffe1fed"
+}
 };
